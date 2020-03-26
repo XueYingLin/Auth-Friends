f323dfa2e6d55a99d50f7352b954b2cb5ff964db
Nikki Lin <nikkiylin@gmail.com>
2020-03-25 19:10:13 -0700

    Added PrivateRoute.js and render it to the app

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import FriendList from './components/FriendList';
 import { AddFriendForm } from './components/AddFriendForm';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -15,11 +16,14 @@ function App() {
             <br />
 
             <Link to="/protected">Protected Page</Link>
+            
       </p>
         <Switch>
-          <Route exact path="/protected" component={FriendList} />
+          <PrivateRoute exact path="/protected" component={FriendList} />
+          
           <Route path="/login" component={Login} />
           <Route component={Login} />
+          
         </Switch>
       </div>
     </Router>
